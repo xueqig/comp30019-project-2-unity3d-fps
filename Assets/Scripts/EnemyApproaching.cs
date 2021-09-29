d17262c56ffb992a8f6463376dd6ccb6659ffdc4
ZhimingDeng5 <zhimingd@student.unimelb.edu.au>
2021-09-29 16:23:11 +0800

    simple enemy approach function

---
+++ Assets/Scripts/EnemyApproaching.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEditor.UIElements;
+using UnityEngine;
+using System.Collections;
+
+namespace DefaultNamespace
+{
+    public class EnemyApproaching : MonoBehaviour
+    {
+        public GameObject Player;
+        public float speed=1;
+        private void Awake()
+        {
+            Player = GameObject.Find("Player");
+        }
+
+        
+        
+        void Update()
+        {
+            Vector3 direction = Player.transform.localPosition - this.transform.localPosition;
+            float distance = (Player.transform.localPosition - this.transform.localPosition).magnitude;
+            float findDistance = 5;
+            if (distance < findDistance)
+            {
+                this.transform.localPosition += direction.normalized * speed * Time.deltaTime;
+            }
+        }
+    }
+}
