c4c59eeb231ea6e7e6cf5c81a5dac7401d8a5e6c
ZhimingDeng5 <zhimingd@student.unimelb.edu.au>
2021-10-04 15:21:16 +0800

    simple enemy model

--- Assets/Scripts/EnemyApproaching.cs
+++ Assets/Scripts/EnemyApproaching.cs
@@ -3,27 +3,47 @@ using UnityEditor.UIElements;
 using UnityEngine;
 using System.Collections;
 
+
 namespace DefaultNamespace
 {
     public class EnemyApproaching : MonoBehaviour
     {
         public GameObject Player;
-        public float speed=1;
+        public float speed;
+        private bool needReset = true;
+
         private void Awake()
         {
             Player = GameObject.Find("Player");
         }
 
         
-        
+
+
         void Update()
         {
-            Vector3 direction = Player.transform.localPosition - this.transform.localPosition;
-            float distance = (Player.transform.localPosition - this.transform.localPosition).magnitude;
+            Vector3 horizontalPosition = new Vector3(Player.transform.localPosition.x, this.transform.localPosition.y,
+                Player.transform.localPosition.z);
+            Vector3 direction = horizontalPosition - this.transform.localPosition;
+            
+            float distance = (horizontalPosition - this.transform.localPosition).magnitude;
             float findDistance = 5;
-            if (distance < findDistance)
+            if (distance<findDistance)
             {
+                this.GetComponent<Idle>().enabled = false;
+                this.transform.forward = direction;
                 this.transform.localPosition += direction.normalized * speed * Time.deltaTime;
+                this.GetComponent<Animator> ().Play ("Walk");
+                needReset = true;
+            }
+            else
+            {
+                if (needReset)
+                {
+                    this.GetComponent<Animator>().Play("idle1", -1, 0f);
+                    this.GetComponent<Idle>().enabled = true;
+                    needReset = false;
+                }
             }
         }
     }
