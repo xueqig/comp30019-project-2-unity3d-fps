22df6ea107cfd3005e89673a4de3288ad5769e7c
Yibo Peng <yibop@student.unimelb.edu.au>
2021-10-25 01:05:13 +0800

    Update PlayerState.cs

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -18,17 +18,36 @@ public class PlayerState : MonoBehaviour
     [SerializeField]
     private int score = 0;
 
+    private int target_score = 0;
+
     public Text scoreUI;
     // Start is called before the first frame update
+
+    void Awake(){
+        
+    }
+
     void Start()
     {
-        
+        GameObject temp = GameObject.Find("Dontdestroy");
+        int level = temp.GetComponent<VariablesSaver>().level;
+        if(level == 1){
+            target_score = 100;
+        }
+        else if(level == 2){
+            target_score = 150;
+        }
+        else if(level == 3){
+            target_score = 300;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(score >= target_score){
+            SceneManager.LoadScene(3);
+        }
     }
 
     public void Health_Change(float healthValue)
