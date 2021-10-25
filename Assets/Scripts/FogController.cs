72e9a47a01e69519f0b8c90616d8de70251862cd
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-10-25 17:20:55 +1100

    add controller to show/hide fog

---
+++ Assets/Scripts/FogController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour
+{
+    public GameObject fog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fog.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
